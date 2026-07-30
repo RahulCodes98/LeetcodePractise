d7fc3f2702814827a3ae202ca42d281461dbba90
Rahul-05 <rahulshankar5590@gmail.com>
2026-07-30 13:13:50 +0530

    Time: 31 ms (68.54%) | Memory: 18.6 MB (90.25%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def detectCycle(self, head):
+        slow = head
+        fast = head
+
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                break
+
+        if fast is None or fast.next is None:
+            return None
+
+        slow = head
+
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+
+        return slow
